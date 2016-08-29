eb17fd13013231a485d7ab8bac19575a5f451270
captain <x169@qq.com>
2016-08-29 21:07:18 +0800

    add ex47 ex48

---
+++ projects/nosetest/test/test.py
@@ -0,0 +1,13 @@
+def setUp():
+	print "function setUp"
+
+def Testfunc1():
+	print "test1"
+	assert True
+
+def Testfunc2():
+	print "test2"
+	assert True
+
+def tearDown():
+	print "function tearDown"
